67d995b517296b39eabe6928065fd1563d294ec9
Jason Engelbrecht <47205928+jason-engelbrecht@users.noreply.github.com>
2020-06-12 14:59:46 -0700

    Update about

--- sections/about.php
+++ sections/about.php
@@ -7,9 +7,9 @@
             <a href="mailto:jason.engelbrecht314@gmail.com">jason.engelbrecht314@gmail.com</a>
         </p>
         <p class="lead mb-5">
-            Fourth-year software development student experienced working in Agile
-            teams on full-stack development projects, seeking a challenging entry-level
-            software engineer position where I am open and eager to continue learning.
+            Recent software development graduate experienced working in Agile teams on development
+            projects in various tech stacks, seeking a challenging entry-level software engineer
+            position where I am open and eager to continue learning.
         </p>
         <div class="social-icons">
             <a href="https://www.linkedin.com/in/jason-engelbrecht/" target="_blank">
