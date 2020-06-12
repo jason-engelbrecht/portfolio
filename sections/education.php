ecad1e9cfc9a0dada990111179577b33f5341a42
Jason Engelbrecht <47205928+jason-engelbrecht@users.noreply.github.com>
2020-06-12 14:29:40 -0700

    Update education

--- sections/education.php
+++ sections/education.php
@@ -4,20 +4,20 @@
         <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
             <div class="resume-content">
                 <h3 class="mb-0">Green River College</h3>
-                <div class="lead mb-1">Bachelor of Applied Science, expected June 2020</div>
-                <div class="mb-3">Software Development (Applied Computer Science), GPA: 3.7</div>
+                <div class="lead mb-1">Bachelor of Applied Science (BAS)</div>
+                <div class="mb-3">Software Development (Applied Computer Science), GPA: 3.8</div>
                 <div class="mt-2"><i class="fas fa-chalkboard-teacher mr-1"></i> Computer Science/Software
-                    Development Tutor, 2020 – Present
+                    Development Tutor
                 </div>
-                <div class="mt-2"><i class="fas fa-user-friends mr-1"></i> South King Software Developer Meetup,
-                    2019 - Present
+                <div class="mt-2"><i class="fas fa-trophy mr-1"></i>
+                    Outstanding Cohort Student Award
                 </div>
-                <div class="mt-2"><i class="fas fa-trophy mr-1"></i> Green River College Foundation Scholarship,
-                    2018 & 2019
+                <div class="mt-2"><i class="fas fa-trophy mr-1"></i>
+                    Green River College Foundation Scholarship
                 </div>
             </div>
             <div class="resume-date text-md-right">
-                <span class="text-primary">2018 - Present</span>
+                <span class="text-primary">September 2018 - June 2020</span>
             </div>
         </div>
     </div>
